54df7889cdf5c4ba5c4e1fa411b14a38dc5ab4ba
Olgaparay <olgaparaeva@hotmail.com>
2024-05-24 16:39:49 +0300

    add catBean Cat and equals

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,21 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean1.equals(bean2));
+        System.out.println(catBean1.equals(catBean2));
     }
 }
